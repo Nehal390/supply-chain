6f5db8b65631891064db5e73ca7a4b4af74bae3a
nehalagarwalnehal@gmail.com <nehalagrawal@Nehals-MacBook-Air.local>
2026-06-14 00:37:23 +0530

    Fix Express 5 catch-all route

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -55,7 +55,7 @@ const frontendPath = path.resolve(
 
 app.use(express.static(frontendPath));
 
-app.get("*", (_req, res) => {
+app.use((_req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 export default app;
